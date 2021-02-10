d9f2f0ca683e8ab6d2786aa579a29e1c75e47ba1
Anthony Valera <anthony@anthonyvalera.com>
2021-02-10 02:06:21 -0800

    Updates eslint config to use react plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "airbnb",
-    "plugins": [
-        "babel"
-    ],
-    "rules": {
-        "babel/new-cap": 1,
-        "babel/no-invalid-this": 1,
-        "babel/semi": 1,
-        "babel/no-unused-expressions": 1,
-        "react/jsx-filename-extension": 0
-    },
-    "env": {
-        "browser": true,
-        "node": true,
-        "jest": true
-    }
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['babel'],
+  rules: {
+    'babel/new-cap': 1,
+    'babel/no-invalid-this': 1,
+    'babel/semi': 1,
+    'babel/no-unused-expressions': 1,
+    'react/jsx-filename-extension': 0,
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
 };
